2108937104abc7f78426b9857158970b802e87e0
Sandeep_Sandy <46670704+SandeepR1305@users.noreply.github.com>
2020-06-01 16:27:34 +0530

    USN updated

--- Usn.py
+++ Usn.py
@@ -1,44 +1,98 @@
 import string
 from os import system
+import time
 
 
 def login():
     system("cls")
     name = input("Enter Student name : ").upper()
-    # usn = input("USN : ").upper()
-    print(f"Which cycle {name} belong's to")
-    num = int(input("1.physics.\n2.chemistry.\nchoise : "))
-    cycle(num)
+    usn = input("USN : ").upper()
+    sems(name, usn)
+    return 0
 
 
-def cycle(num):
+def sems(name, usn):
+    system("cls")
+    print(f"Which SEMESTER {name} belong's to ")
+    num = int(
+        input(
+            "1 : Semester.\n2 : Semester.\n3 : Semester.\n4 : Semester.\n5 : Semester.\n6 : Semester.\n7 : Semester.\n8 : Semester.\nchoise : "
+        )
+    )
+    if num == 1:
+        print("FIRST SEMESTER\n")
+        sem(name, usn)
+    elif num == 2:
+        print("SECOND SEMESTER\n")
+        sem(name, usn)
+    elif num == 3:
+        cyl_name = "Third SEM"
+        subject = [
+            "M3",
+            "Data Structures and Applications",
+            "Analog and Digital Electronics",
+            "Computer Organization",
+            "Software Engineering",
+            "Discrete Mathematical Structures",
+        ]
+        sub_code = ["18MAT31", "18CS32", "18CS33", "18CS34", "18CS35", "18CS36"]
+        sub_item = Sub_push(subject, cyl_name, sub_code)
+        ptr(sub_item, name, usn)
+        return 0
+    elif num == 4:
+        cyl_name = "Forth SEM"
+        return 0
+    else:
+        pass
+
+
+def sem(name, usn):
+    system("cls")
+    print(f"Which SEMESTER {name} belong's to ")
+    num = int(input("1.Physics cycle.\n2.Chemistry cycle.\nchoise : "))
     if num == 1:
         cyl_name = "Physics cycle"
-        subject = ["M1", "Physics", "Electrical", "English"]
-        len_sub = len(subject)
-        T_sub_marks = push_sub(subject, len_sub, cyl_name)
-        print(T_sub_marks)
-        # ptr(T_sub_marks, subject, cyl_name)
+        subject = ["M1", "Physics", "Civil", "Electrical", "English"]
+        sub_code = ["18MAT11", "18PHY12", "18CIV14", "18ELE13", "18EGH18"]
+        sub_item = Sub_push(subject, cyl_name, sub_code)
+        ptr(sub_item, name, usn)
+        return 0
 
     else:
         cyl_name = "Chemistry cycle"
-        subject = ["M1", "Chemistry", "Electronics", "Computer science", "English"]
-        len_sub = len(subject)
-        T_sub_marks = push_sub(subject, len_sub, cyl_name)
-        ptr(T_sub_marks, subject, cyl_name)
+        subject = [
+            "M1",
+            "Chemistry",
+            "Electronics",
+            "Computer science",
+            "Mechanical",
+            "English",
+        ]
+        sub_code = ["18MAT21", "18CHE22", "18ELN24", "18CPS23", "18ME25", "18EGH28"]
+        sub_item = Sub_push(subject, cyl_name, sub_code)
+        ptr(sub_item, name, usn)
+        return 0
 
 
-def push_sub(subject, len_sub, cyl_name):
+def Sub_push(subject, cyl_name, sub_code):
+    T_sub_marks = push_sub(subject, cyl_name, sub_code)
+    return T_sub_marks
+
+
+def push_sub(subject, cyl_name, sub_code):
     Ia_marks = []
     T_sub = []
-    for item in subject:
-        Ia_marks = marks(item, len_sub, cyl_name)
-        ass_ = ass(item)
-        T_sub.append(cal(Ia_marks, ass_, subject))
+    merge_list = tuple(zip(subject, sub_code))
+    for i in merge_list:
+        j = list(i)
+        Ia_marks = marks(j[1], cyl_name, j[0])
+        ass_ = ass(j[1])
+        T_sub.append(cal(Ia_marks, ass_, j[0], j[1]))
+
     return T_sub
 
 
-def marks(subject_list, len_sub, cyl_name):
+def marks(subject_list, cyl_name, sub_code):
     system("cls")
     print(cyl_name)
     print(f"Enter 3 internal {subject_list} marks")
@@ -57,8 +111,8 @@ def marks(subject_list, len_sub, cyl_name):
     return tmarks, avg
 
 
-def ass(subject):
-    print(f"Enter {subject} Assigment marks : ", end="")
+def ass(sub_code):
+    print(f"Enter {sub_code} Assigment marks : ", end="")
     Ass_marks = int(input())
     while Ass_marks > 10:
         system("cls")
@@ -68,39 +122,52 @@ def ass(subject):
     return Ass_marks
 
 
-def cal(Ia_marks, assi, subject):
+def cal(Ia_marks, assi, subject, sub_codes):
     sub_marks = []
     avg_marks = []
     ass_marks = []
     sub = []
+    sub_code = []
     sub_marks.append(Ia_marks[0])
     avg_marks.append(round(Ia_marks[1], 2))
     ass_marks.append(assi)
     sub.append(subject)
+    sub_code.append(sub_codes)
 
-    return sub_marks, avg_marks, ass_marks, sub
+    return sub_marks, avg_marks, ass_marks, sub, sub_code
 
 
-def ptr(T_sub_marks, subject, cyl_name):
-    print("SUBJECT       T_MARKS       T_AVG       ASSIMENT")
-    for item in subject:
-        t_sub = list(T_sub_marks)
-        i = 0
-        for i in t_sub:
-            j = list(i)
-            print(f"{item}   :   {j[0]}   :   {j[1]}   :   {j[2]}")
+def ptr(T_sub_marks, name, usn):
+    system("cls")
+    print("\n\t\t\tName of the student : ", name)
+    print("\t\t\tUSN : ", usn)
+    print("\t\t\tMarks of the Student\n")
+    for item in T_sub_marks:
+        j = list(item)
+        print(
+            "Subject code  : ",
+            *j[4],
+            "\nSubject       : ",
+            *j[3],
+            "\nTotal marks   : ",
+            *j[0],
+            "\nAverage marks : ",
+            *j[1],
+        )
+        print("\n")
 
 
 # main
 login()
 
 
-"""print("Login in into your account.")
-log = input("login ID : ")
-pwd = input("password : ")
+"""
+    print("Login in into your account.")
+    log = input("login ID : ")
+    pwd = input("password : ")
 
-if log == "log!@#" and pwd == "qwerty":
-    print("welcome")
-else:
+    if log == "log!@#" and pwd == "qwerty":
+        print("welcome")
+    else:
     print("invalid")
 """
